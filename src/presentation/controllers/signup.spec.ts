86141ab2d0f182d9853e15f3aec0a3b6b48ccaf3
João Martins <jvmartinsdev@gmail.com>
2023-02-13 23:05:22 -0300

    test: ensure SignUpController validates payload

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,34 @@
+import { SignUpController } from './signup'
+import { MissingParamError } from '../errors/missing-params-error'
+
+describe('SignUpController', () => {
+  test('Should return 400 if no name is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('Missing param: name'))
+  })
+
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'any_name@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('Missing param: name'))
+  })
+})
